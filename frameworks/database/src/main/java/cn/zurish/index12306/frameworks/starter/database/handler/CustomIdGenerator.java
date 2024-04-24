64cd8daa858dfa69f01141f02a9251d65320bcbc
wind <zurish@qq.com>
2024-04-24 19:33:38 +0800

    feature: 开发基础设施底层依赖持久层模块

---
+++ frameworks/database/src/main/java/cn/zurish/index12306/frameworks/starter/database/handler/CustomIdGenerator.java
@@ -0,0 +1,15 @@
+package cn.zurish.index12306.frameworks.starter.database.handler;
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+
+/**
+ * 自定义雪花算法生成器
+ * 2024/4/24 下午7:20
+ */
+public class CustomIdGenerator implements IdentifierGenerator {
+
+    @Override
+    public Number nextId(Object entity) {
+        return SnowflakeIdUtil.nextId();
+    }
+}
