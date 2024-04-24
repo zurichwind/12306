a7b7e8781138ffa000c128d8b286aa36f2249a20
wind <zurish@qq.com>
2024-04-24 19:32:32 +0800

    feature: 开发基础设施底层依赖设计模式模块

---
+++ frameworks/designpattern/src/main/java/cn/zurish/index12306/frameworks/starter/designpattern/chain/AbstractChainHandler.java
@@ -0,0 +1,22 @@
+package cn.zurish.index12306.frameworks.starter.designpattern.chain;
+
+import org.springframework.core.Ordered;
+
+/**
+ * 抽象业务责任链组件
+ * 2024/4/24 下午5:33
+ */
+public interface AbstractChainHandler<T> extends Ordered {
+
+    /**
+     * 执行责任链逻辑
+     *
+     * @param requestParam 责任链执行入参
+     */
+    void handler(T requestParam);
+
+    /**
+     * @return 责任链组件标识
+     */
+    String mark();
+}
